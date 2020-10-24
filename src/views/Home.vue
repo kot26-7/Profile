803d75379ea543f17f755add9e55c5c6f85c7c66
kot26-7 <taro726896oka@gmail.com>
2020-10-25 00:37:48 +0900

    add by-name in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,5 +3,12 @@
     <vue-headful title="Home - Myprofile"/>
     <h1 class="main-title">Thank you for coming.</h1>
     <h1 class="main-title">Please have a look.</h1>
+    <h2 class="by-name">by kot26-7</h2>
   </div>
 </template>
+
+<style scoped>
+  .by-name {
+    margin-top: 30px;
+  }
+</style>
