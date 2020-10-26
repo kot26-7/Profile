437a7f9af81721c47e036976d93f3baae4d53a62
kot26-7 <taro726896oka@gmail.com>
2020-10-26 18:30:56 +0900

    ready to deploy

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,14 +1,13 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyDu18ImAdaZggZNBDwhLP8qI5fK8KMXW2E",
-  authDomain: "sending-email-task.firebaseapp.com",
-  databaseURL: "https://sending-email-task.firebaseio.com",
-  projectId: "sending-email-task",
-  storageBucket: "sending-email-task.appspot.com",
-  messagingSenderId: "37936632689",
-  appId: "1:37936632689:web:3b93990a8ff27437511cd0",
-  measurementId: "G-TEREYY6CT2"
+  apiKey: "AIzaSyA2eKx5yHZZhycKc6WsOpZkCkT_RH1SdGU",
+  authDomain: "kot-profile-267.firebaseapp.com",
+  databaseURL: "https://kot-profile-267.firebaseio.com",
+  projectId: "kot-profile-267",
+  storageBucket: "kot-profile-267.appspot.com",
+  messagingSenderId: "842253242502",
+  appId: "1:842253242502:web:4f5f0b5d5cb9fde59e0107",
 };
 firebase.initializeApp(config);
 export const functions = firebase.functions();
